5d5b671eec3054d40359a535352d0f868be30e97
XIAO Jun <xiaojunshcn@gmail.com>
2014-01-29 14:31:50 +0800

    first commit

---
+++ JQuerySkills/js/jquery.weibo.map.js
@@ -0,0 +1,322 @@
+﻿(function ($) {
+    $.fn.weiboMap = function (options) {
+        'use strict';
+        // Version = '0.0.0.1';
+        var opts = $.extend(true, {}, options, $.fn.weiboMap.defaults),
+        ey, my, mouseDown = false, geoCoder;
+
+
+        $.fn.weiboMap.init();
+
+        // Gets the list of weibo screen name
+        // from the .get div.
+        function getUsers() {
+            var arr = new Array();
+            $('.get').find('input').each(function (i) {
+                var $element = $(this);
+                arr[i] = $element.val();
+            });
+            return arr;
+        }
+
+        show();
+
+        // Get weibo user show and binding data
+        // with google map.
+        function show() {
+
+            // Gets the weibo user screen name.
+            var users = getUsers();
+            for (var i = users.length - 1; i >= 0; i--) {
+
+                // Invokes the weibo api to get data.
+                $.getJSONP({
+                    url: opts.url,
+                    timeout: 30000,
+                    data: {
+                        source: opts.appKey,
+                        access_token: opts.accessToken,
+                        screen_name: users[i]
+                    },
+                    error: function (xhr, status, e) {
+                        console.log(e);
+                    },
+                    complete: function () {
+
+                    },
+                    success: function (json) {
+                        if (json.data.error) {
+                            // log the error.
+                            return;
+                        }
+
+                        var arr = new Array(),
+                            img = json.data.profile_image_url,
+                            screen_name = json.data.screen_name;
+
+                        // Initalizes the geo coder instance.    
+                        geocoder.geocode({
+                            address: json.data.location
+                        }, function (response, status) {
+                            if (status == google.maps.GeocoderStatus.OK) {
+
+                                // Sets latitude and longitude by location name.
+                                var x = response[0].geometry.location.lat(),
+                                    y = response[0].geometry.location.lng(),
+                                    blogUrl,
+                                marker = new google.maps.Marker({
+                                    icon: img,
+                                    map: map,
+                                    title: screen_name,
+                                    animation: google.maps.Animation.DROP,
+                                    position: new google.maps.LatLng(x, y)
+                                });
+
+                                // Creates user information.
+                                blogUrl = json.data.url !== '' ? json.data.url : 'http://www.weibo.com/u/' + json.data.id;
+                                arr.push('<div class="item">');
+                                arr.push('<p class="img"><a href="#" class="open" rel="' + screen_name + '"><img src="' + img + '" alt="" /></a></p>');
+                                arr.push('<div class="entry">');
+                                arr.push('<a href="#" class="open title" rel="' + screen_name + '">' + json.data.name + '</a>');
+                                arr.push('<p class="description">' + json.data.description + '</p>');
+                                arr.push('<p class="url"><a href="' + blogUrl + '" target="_blank">' + blogUrl + '</a></p>');
+                                arr.push('<p class="count">粉丝: ' + json.data.followers_count + ', 关注: ' + json.data.friends_count + '</p>');
+                                arr.push('</div>');
+                                arr.push('</div>');
+                                var html = arr.join('');
+                                arr = [];
+                                $('.weibo').find('.inside').append(html);
+
+                                // Clicks the user image showing relative user's weibo.
+                                google.maps.event.addListener(marker, 'click', function () {
+                                    open(this.title);
+                                });
+                            }
+
+                        });
+                    }
+                });
+            };
+
+        }
+
+        // When click the user image, then showing the user weibo.
+        function click() {
+            // toggleBounce();
+            $('.weibo').find('.open').live('click', function () {
+                var t = $(this),
+                    rel = t.attr('rel');
+                open(rel);
+            });
+        }
+
+        // Displays the user weibo.
+        function open(user) {
+            var posts = $('.posts'),
+                arr = new Array;
+            $.getJSONP({
+                url: opts.urlTimeline,
+                timeout: 30000,
+                data: {
+                    source: opts.appKey,
+                    access_token: opts.accessToken,
+                    screen_name: user
+                },
+                error: function (xhr, status, e) {
+                    console.log(e);
+                },
+                complete: function () {
+
+                },
+                success: function (json) {
+                    // Displays the weibo in post div.
+                    $.each(json.data.statuses, function (i, post) {
+                        arr.push('<div class="post">');
+                        arr.push(post.text);
+                        arr.push('</div>');
+                    });
+                    var html = arr.join('');
+                    posts.html(html).fadeIn();
+                }
+            });
+        }
+
+        // Gets mouse Y coordinate.
+        function getYCoordinate(e) {
+            var y = e.pageY;
+            return y;
+        }
+
+        // Checks move size.
+        function checkYCoordinate(y) {
+            var all = $('.weibo').height(),
+                inside = $('.weibo').find('.inside').height();
+            // The max move size |all - inside|.    
+            if (y < (all - inside)) {
+                y = all - inside;
+            } else if (y > 0) {
+                y = 0;
+            }
+            return y;
+        }
+
+        // Updates inside top css.
+        function update(e) {
+            // Gets the current Y coordinate.
+            var y = getYCoordinate(e),
+                // The move size, if move up movey < 0,
+                // otherwise movey >= 0.
+                movey = y - my,
+
+                // Changes Y coordinate.
+                top = ey + movey,
+            check = checkYCoordinate(top);
+            console.log(top, check);
+            $('.weibo').find('.inside').css({
+                top: check + 'px'
+            });
+        }
+
+        init();
+
+        function init() {
+            $('.weibo').find('.inside').bind({
+                mousedown: function (e) {
+                    e.preventDefault();
+                    mouseDown = true;
+                    var mouseY = getYCoordinate(e),
+                    // Gets element coordinate.
+                    element = $(this).position();
+
+                    // Gets Y coordinate move.
+                    my = mouseY;
+                    ey = element.top;
+                    update(e);
+                },
+                mousemove: function (e) {
+                    if (mouseDown)
+                        update(e);
+                    return false;
+                },
+                mouseup: function () {
+                    if (mouseDown)
+                        mouseDown = false;
+                    return false;
+                },
+                mouseleave: function () {
+                    if (mouseDown)
+                        mouseDown = false;
+                    return false;
+                }
+            });
+        }
+    };
+
+    // Gets the size of window .
+    $.fn.weiboMap.size = function () {
+        var w = $(window).width(),
+            h = $(window).height();
+        return {
+            width: w,
+            height: h
+        };
+    }
+
+    // Initalizes the google map
+    $.fn.weiboMap.init = function () {
+
+        // Sets the size of map.
+        var size = $.fn.weiboMap.size();
+        $('#map').css({
+            width: size.width,
+            height: size.height
+        });
+
+        // Creates a google map instance.
+        map = new google.maps.Map(document.getElementById('map'), $.fn.weiboMap.defaults.data);
+
+        // Creates a geo coder instance.
+        geocoder = new google.maps.Geocoder();
+        google.maps.event.addListener(map, 'dragstart', function () {
+            $('.posts').hide();
+        });
+    }
+
+    $.fn.weiboMap.defaults = {
+        url: 'https://api.weibo.com/2/users/show.json?',
+        urlTimeline: 'https://api.weibo.com/2/statuses/user_timeline.json?',
+        appKey: '82966982',
+        accessToken: '5Jao51NF1i5PDC91hhI3ID86ucoDtn4C',
+        uid: '',
+        screenName: '',
+        data: {
+            zoom: 4,
+            center: new google.maps.LatLng(39, 115), // Sets map init center location.
+            mapTypeId: google.maps.MapTypeId.ROADMAP
+        }
+    };
+
+
+    /**
+     * Sends cross origin request with ajax and jsonp.
+     * @param s
+     * The params need to send to server.
+     * For instance, url, appkey, q or count and so forth.
+     */
+    $.getJSONP = function (s) {
+
+        // Due to cross origin request, so we to use jsonp format.
+        s.dataType = "jsonp";
+        $.ajax(s);
+
+        // figure out what the callback fn is
+        var $script = $(document.getElementsByTagName('head')[0].firstChild);
+        var url = $script.attr('src') || '';
+
+        // Gets callback function
+        var cb = (url.match(/callback=(\w+)/) || [])[1];
+
+        if (!cb)
+            return; // bail
+        var t = 0,
+            cbFn = window[cb];
+
+        $script[0].onerror = function (e) {
+            $script.remove();
+            handleError(s, {}, "error", e);
+            clearTimeout(t);
+        };
+
+        if (!s.timeout)
+            return;
+
+        window[cb] = function (json) {
+            clearTimeout(t);
+            cbFn(json);
+            cbFn = null;
+        };
+
+        // Gets time out function flag.
+        t = setTimeout(function () {
+            $script.remove();
+            handleError(s, {}, "timeout");
+            if (cbFn)
+                window[cb] = function () {};
+        }, s.timeout);
+
+        /**
+         * Fix issue: "jQuery.handleError is not a function"
+         * @param s
+         * @param xhr
+         * @param msg
+         * @param e
+         */
+
+        function handleError(s, xhr, msg, e) {
+            s.error && s.error.call(s.context, xhr, msg, e);
+            s.global && $.event.trigger("ajaxError", [xhr, s, e || msg]);
+            s.complete && s.complete.call(s.context, xhr, e || msg);
+        }
+    };
+
+})(jQuery);
---
+++ JQuerySkills/readme.txt
@@ -0,0 +1,42 @@
+01.html		使用JavaScript 实现隔行变色
+02.html		使用jQuery 选择器实现隔行变色
+
+03.html		使用jQuery 基本选择器
+04.html		使用jQuery 层次选择器
+05.html		使用jQuery 基本过滤选择器选择元素
+06.html		使用jQuery 内容过滤选择器选择元素
+07.html		使用jQuery 可见性过滤选择器选择元素
+08.html		使用jQuery 属性过滤选择器选择元素
+09.html		使用jQuery 子元素过滤选择器选择元素
+10.html		使用jQuery 表单对象属性过滤选择器选择元素
+11.html		使用jQuery 表单过滤选择器获取元素
+12.html		导航条在项目中的应用
+
+13.html		动态创建节点元素
+14.html		使用clone() 方法复制元素节点
+15.html		包裹外部元素和内部文本的方法
+16.html		遍历元素获取属性
+17.html		删除页面中指定的元素
+18.html		数据删除和图片预览在项目中的应用
+
+19.html		放大镜功能(鼠标移动到的区域局部放大)---原图之上局部放大
+20.html		即时调用新浪微博的查询接口返回纪录
+21.html		Google Map应用 + Weibo应用
+22.html		3D式样 图片切换 轮回（点击左右按钮切换）
+23.html		3D式样 图片切换 轮回（定时切换）
+24.html		淘宝式放大镜---在原图片区域外放大
+25.html		淘宝式放大镜(无第三方js)
+
+26.html		检测不同浏览器下的鼠标滚轮事件
+27.html		多图片切换轮播,Slide show
+
+28.html		当前页上任何区域加loading动画（开源的）
+29.html		当前页上覆盖一loading动画层(针对id, class属性)
+
+
+
+
+
+
+
+
